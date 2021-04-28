aa47043a41296274c382d0d73033d59392385248
kirito <18624275868@163.com>
2021-04-28 17:53:47 +0800

    validator

--- example/blog/go.mod
+++ example/blog/go.mod
@@ -4,7 +4,10 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.7.1
+	github.com/go-playground/locales v0.13.0
+	github.com/go-playground/universal-translator v0.17.0
 	github.com/jinzhu/gorm v1.9.16
 	github.com/spf13/viper v1.7.1
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 )
--- example/blog/internal/routers/router.go
+++ example/blog/internal/routers/router.go
@@ -1,6 +1,7 @@
 package routers
 
 import (
+	"blog/internal/middleware"
 	v1 "blog/internal/routers/api/v1"
 	"github.com/gin-gonic/gin"
 )
@@ -9,6 +10,7 @@ func NewRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	r.Use(middleware.Translations())
 	apiv1 := r.Group("api/v1/")
 	{
 		tag := v1.NewTag()
