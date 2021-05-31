8e2928dfa54ab5705affb0fef9c3bd9e3d58c010
jojo-jie <18624275868@163.com>
2021-05-31 15:28:19 +0800

    zone copy

--- example/base/server/server_test.go
+++ example/base/server/server_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"reflect"
 	"testing"
+	"unsafe"
 )
 
 type Message struct {
@@ -52,4 +54,40 @@ func NewByOption(opts ...Option) Message {
 func Test_option(t *testing.T) {
 	// 如何理解函数式选项模式
 	t.Log(NewByOption(WithID(2), WithName("message2"), WithAddress("cache2"), WithPhone(456)))
+}
+
+var s string
+
+func init()  {
+	s = "脑子进煎鱼了"
+}
+
+func BenchmarkTwoHeader(b *testing.B) {
+	b.ResetTimer()
+	for i:=0;i<b.N;i++ {
+		stringToByte(s)
+	}
+}
+
+func BenchmarkOld(b *testing.B) {
+	b.ResetTimer()
+	for i:=0;i<b.N;i++ {
+		stringToByteO(s)
+	}
+}
+
+
+// 0拷贝
+func stringToByte(s string) []byte {
+	stringHeader := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	var b []byte
+	sliceHeader := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	sliceHeader.Data = stringHeader.Data
+	sliceHeader.Len = stringHeader.Len
+	sliceHeader.Cap = stringHeader.Len
+	return b
+}
+
+func stringToByteO(s string) []byte {
+	return []byte(s)
 }
