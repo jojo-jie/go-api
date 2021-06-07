e8df2eec38b9437c1a5b0e9f02583e2c67439c8f
jojo-jie <18624275868@163.com>
2021-06-07 22:11:01 +0800

    grpc interceptor

--- example/tag-service/client/main.go
+++ example/tag-service/client/main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"context"
 	"encoding/json"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"log"
+	"tag-service/internal/middleware"
 	"tag-service/pkg/errcode"
 	pb "tag-service/proto"
 )
@@ -16,24 +20,41 @@ func main() {
 		log.Fatalf("err: %v", err)
 	}
 	defer clientConn.Close()
-	tagServiceClient:=pb.NewTagServiceClient(clientConn)
+	tagServiceClient := pb.NewTagServiceClient(clientConn)
 	resp, err := tagServiceClient.GetTagList(ctx, &pb.GetTagListRequest{
 		Name: "Go",
 	})
 	if err != nil {
-		err:=errcode.TogRPCError(errcode.ErrorGetTagListFail)
-		sts:= errcode.FromError(err)
-		details:=sts.Details()
-		detail := details[0].(*pb.Error)
-		// 客户端内部业务错误码
-		log.Fatalf("tagServiceClient.GetTagList err:%v code:%d msg:%s", details, detail.Code, detail.Message)
+		sts := errcode.FromError(err)
+		details := sts.Details()
+		if len(details) > 1 {
+			detail := details[0].(*pb.Error)
+			// 客户端内部业务错误码
+			log.Fatalf("tagServiceClient.GetTagList err:%v code:%d msg:%s", details, detail.Code, detail.Message)
+		}
+		if sts.Code() == codes.DeadlineExceeded {
+			log.Fatalf("%s", "timeout")
+		}
 	}
 	log.Printf("resp %v", resp)
-	body,_:=json.Marshal(resp)
+	body, _ := json.Marshal(resp)
 	log.Printf("resp %s", string(body))
 }
 
 func GetClientConn(ctx context.Context, target string, opt []grpc.DialOption) (*grpc.ClientConn, error) {
-	opt = append(opt, grpc.WithInsecure())
-	return grpc.DialContext(ctx, target, opt...)
+	opts := append(opt, grpc.WithInsecure())
+	opts = append(opts, grpc.WithChainUnaryInterceptor(
+		grpc_middleware.ChainUnaryClient(middleware.UnaryContextTimeout(), grpc_retry.UnaryClientInterceptor(
+			grpc_retry.WithMax(2),
+			grpc_retry.WithCodes(
+				codes.Unknown,
+				codes.Internal,
+				codes.DeadlineExceeded,
+			),
+		)),
+	))
+	opts = append(opts, grpc.WithChainStreamInterceptor(
+		grpc_middleware.ChainStreamClient(middleware.StreamContextTimeout()),
+	))
+	return grpc.DialContext(ctx, target, opts...)
 }
