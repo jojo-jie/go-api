67149d5e1869bdf1867c641086ba6860e8e57f19
jojo-jie <18624275868@163.com>
2022-03-23 19:54:52 +0800

    quicksort

--- example/dayday/one_test.go
+++ example/dayday/one_test.go
@@ -1,6 +1,10 @@
 package dayday
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
 
 // https://mp.weixin.qq.com/s/0nChJVXxhFYDu453e7Z8Hg
 func TestCC(t *testing.T) {
@@ -30,4 +34,49 @@ func ccc() (func(), error) {
 		print("ccc:surprise!")
 		done()
 	}, nil
+}
+
+func TestQuickSort(t *testing.T) {
+	rand.Seed(time.Now().Unix())
+	sequence := rand.Perm(34)
+	t.Logf("sequence before sort: %v", sequence)
+	quickSort(sequence, 0, len(sequence)-1)
+	t.Logf("sequence after sort: %v", sequence)
+}
+
+func quickSort(sequence []int, low int, high int) {
+	if high <= low {
+		return
+	}
+	j := partition(sequence, low, high)
+	quickSort(sequence, low, j-1)
+	quickSort(sequence, j+1, high)
+}
+
+// first quicksort
+func partition(sequence []int, low, high int) int {
+	i, j := low+1, high
+	for {
+		// first element is pivot
+		for sequence[i] < sequence[low] {
+			i++
+			if i >= high {
+				break
+			}
+		}
+
+		for sequence[j] > sequence[low] {
+			j--
+			if j <= low {
+				break
+			}
+		}
+
+		if i >= j {
+			break
+		}
+		sequence[i], sequence[j] = sequence[j], sequence[i]
+	}
+	sequence[low], sequence[j] = sequence[j], sequence[low]
+	return j
 }
