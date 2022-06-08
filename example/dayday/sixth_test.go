23adc238ba11134cfb099959cee790c2852ec1b3
jojo-jie <18624275868@163.com>
2022-06-08 17:36:06 +0800

    heartbeat

--- example/dayday/sixth_test.go
+++ example/dayday/sixth_test.go
@@ -3,6 +3,7 @@ package dayday
 import (
 	"math/rand"
 	"testing"
+	"time"
 )
 
 func TestGener(t *testing.T) {
@@ -70,5 +71,75 @@ func TestGener(t *testing.T) {
 
 	for num := range take(done, repeatFn(done, rand), 10) {
 		t.Logf("%v ", num)
+	}
+}
+
+func TestHeart(t *testing.T) {
+	doWork := func(done <-chan interface{}, pulseInterval time.Duration) (<-chan interface{}, <-chan time.Time) {
+		heartbeat := make(chan interface{})
+		results := make(chan time.Time)
+		go func() {
+			defer close(heartbeat)
+			defer close(results)
+			pulse := time.Tick(pulseInterval)
+			workGen := time.Tick(pulseInterval * 2)
+			sendPulse := func() {
+				select {
+				case heartbeat <- struct{}{}:
+				default:
+				}
+			}
+
+			sendResult := func(r time.Time) {
+				for {
+					select {
+					case <-done:
+						return
+					case <-pulse:
+						sendPulse()
+					case results <- r:
+						return
+					}
+				}
+			}
+
+			for {
+				select {
+				case <-done:
+					return
+				case <-pulse:
+					sendPulse()
+				case r := <-workGen:
+					sendResult(r)
+				}
+			}
+		}()
+
+		return heartbeat, results
+	}
+
+	done := make(chan interface{})
+	time.AfterFunc(10*time.Second, func() {
+		close(done)
+	})
+
+	const timeout = 2 * time.Second
+
+	heartbeat, results := doWork(done, timeout/2)
+	for {
+		select {
+		case _, ok := <-heartbeat:
+			if !ok {
+				return
+			}
+			t.Log("pulse")
+		case r, ok := <-results:
+			if !ok {
+				return
+			}
+			t.Logf("result %v\n", r.Second())
+		case <-time.After(timeout):
+			return
+		}
 	}
 }
