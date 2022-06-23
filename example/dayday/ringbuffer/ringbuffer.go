cb22ee1e9e476bb8d41a563fbb019854b0401504
jojo-jie <18624275868@163.com>
2022-06-23 11:33:23 +0800

    ringbuffer

--- example/dayday/ringbuffer/ringbuffer.go
+++ example/dayday/ringbuffer/ringbuffer.go
@@ -34,5 +34,85 @@ func (r *RingBuffer[T]) Read() (T, error) {
 	if r.r == r.w {
 		return t, ErrIsEmpty
 	}
-	return nil,nil
+	v := r.buf[r.r]
+	r.r++
+	if r.r == r.size {
+		r.r = 0
+	}
+	return v, nil
 }
+
+func (r *RingBuffer[T]) Pop() T {
+	v, err := r.Read()
+	if err == ErrIsEmpty {
+		panic(ErrIsEmpty.Error())
+	}
+	return v
+}
+
+func (r *RingBuffer[T]) Peek() T {
+	if r.r == r.w {
+		panic(ErrIsEmpty.Error())
+	}
+	v := r.buf[r.r]
+	return v
+}
+
+func (r *RingBuffer[T]) Write(v T) {
+	r.buf[r.w] = v
+	r.w++
+	if r.w == r.size {
+		r.w = 0
+	}
+
+	if r.w == r.r {
+		r.grow()
+	}
+}
+
+func (r *RingBuffer[T]) grow() {
+	var size int
+	if r.size < 1024 {
+		size = r.size * 2
+	} else {
+		size = r.size + r.size/4
+	}
+
+	buf := make([]T, size)
+	copy(buf[0:], r.buf[r.r:])
+	copy(buf[r.size-r.r:], r.buf[0:r.r])
+
+	r.r = 0
+	r.w = r.size
+	r.size = size
+	r.buf = buf
+}
+
+func (r *RingBuffer[T]) IsEmpty() bool {
+	return r.r == r.w
+}
+
+// Capacity returns the size of the underlying buffer.
+func (r *RingBuffer[T]) Capacity() int {
+	return r.size
+}
+
+func (r *RingBuffer[T]) Len() int {
+	if r.r == r.w {
+		return 0
+	}
+
+	if r.w > r.r {
+		return r.w - r.r
+	}
+
+	return r.size - r.r + r.w
+}
+
+func (r *RingBuffer[T]) Reset() {
+	r.r = 0
+	r.w = 0
+	r.size = r.initialSize
+	r.buf = make([]T, r.initialSize)
+}
+
