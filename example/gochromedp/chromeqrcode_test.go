e0a6f1ccbfd41564321716c27330fc179e97a753
jojo-jie <18624275868@163.com>
2022-06-29 14:52:14 +0800

    check login

--- example/gochromedp/chromeqrcode_test.go
+++ example/gochromedp/chromeqrcode_test.go
@@ -3,13 +3,16 @@ package gochromedp
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
+	"github.com/chromedp/cdproto/network"
 	"github.com/chromedp/chromedp"
 	"github.com/makiuchi-d/gozxing"
 	"github.com/makiuchi-d/gozxing/qrcode"
 	goQrcode "github.com/skip2/go-qrcode"
 	"image"
-	"io/ioutil"
+	"os"
+	"strings"
 	"testing"
 	"time"
 )
@@ -22,17 +25,33 @@ func TestQrcodeLogin(t *testing.T) {
 			chromedp.Flag("headless", false),
 		)...,
 	)
-	ctx, _ = context.WithTimeout(ctx, 30*time.Second)
+	ctx, _ = context.WithTimeout(ctx, 300*time.Second)
 	ctx, _ = chromedp.NewContext(ctx, chromedp.WithDebugf(t.Logf))
 	//defer cancel()
 	if err := chromedp.Run(ctx, myTasks()); err != nil {
-		t.Fatal(err)
+		err := chromedp.Run(ctx, myReadyLogin())
+		if err != nil {
+			t.Fatal(err)
+		}
+	} else {
+		t.Log("success !!!!")
 	}
+
 }
 
 const loginURL = "https://account.wps.cn/"
 
+const userCenterURl = "https://account.wps.cn/usercenter/apps"
+
 func myTasks() chromedp.Tasks {
+	return chromedp.Tasks{
+		loadCookies(),
+		chromedp.Navigate(userCenterURl),
+		checkLoginStatus(),
+	}
+}
+
+func myReadyLogin() chromedp.Tasks {
 	return chromedp.Tasks{
 		// open login view
 		chromedp.Navigate(loginURL),
@@ -40,8 +59,8 @@ func myTasks() chromedp.Tasks {
 		chromedp.Click(`#wechat > span:nth-child(2)`),
 		// click confirm button
 		chromedp.Click(`#dialog > div.dialog-wrapper > div > div.dialog-footer > div.dialog-footer-ok`),
-
 		getCode(),
+		saveCookies(),
 	}
 }
 
@@ -51,8 +70,8 @@ func getCode() chromedp.ActionFunc {
 		if err = chromedp.Screenshot(`#wximport`, &code, chromedp.ByID).Do(ctx); err != nil {
 			return
 		}
-		return printQRCode(code)
-		//return saveQRCodeImg(code)
+		//return printQRCode(code)
+		return saveQRCodeImg(code)
 	}
 }
 
@@ -78,5 +97,59 @@ func printQRCode(code []byte) (err error) {
 }
 
 func saveQRCodeImg(code []byte) (err error) {
-	return ioutil.WriteFile("code.png", code, 0755)
+	return os.WriteFile("code.png", code, 0755)
+}
+
+func saveCookies() chromedp.ActionFunc {
+	return func(ctx context.Context) (err error) {
+		// wait qrcode login
+		fmt.Println("wait qrcode login")
+		if err = chromedp.WaitVisible(`#app`, chromedp.ByID).Do(ctx); err != nil {
+			return
+		}
+		cookies, err := network.GetAllCookies().Do(ctx)
+		if err != nil {
+			return
+		}
+		cookiesData, err := network.GetAllCookiesReturns{Cookies: cookies}.MarshalJSON()
+		if err != nil {
+			return
+		}
+		// save tmp file
+		if err = os.WriteFile("cookies.tmp", cookiesData, 0755); err != nil {
+			return
+		}
+		return
+	}
+}
+
+func loadCookies() chromedp.ActionFunc {
+	return func(ctx context.Context) (err error) {
+		if _, err = os.Stat("cookies.tmp"); err != nil {
+			return
+		}
+		cookiesData, err := os.ReadFile("cookies.tmp")
+		if err != nil {
+			return
+		}
+		cookiesParams := network.SetCookiesParams{}
+		if err = cookiesParams.UnmarshalJSON(cookiesData); err != nil {
+			return
+		}
+		return network.SetCookies(cookiesParams.Cookies).Do(ctx)
+	}
+}
+
+func checkLoginStatus() chromedp.ActionFunc {
+	return func(ctx context.Context) (err error) {
+		var url string
+		if err = chromedp.Evaluate(`window.location.href`, &url).Do(ctx); err != nil {
+			return
+		}
+		if strings.Contains(url, userCenterURl) {
+			fmt.Println("login success")
+			return nil
+		}
+		return errors.New("login fail")
+	}
 }
--- example/gochromedp/go.mod
+++ example/gochromedp/go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/chromedp/chromedp v0.8.2
 	github.com/go-echarts/go-echarts/v2 v2.2.4
 	github.com/makiuchi-d/gozxing v0.1.1
+	github.com/pkg/errors v0.9.1
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 )
 
 require (
@@ -16,7 +18,6 @@ require (
 	github.com/gobwas/ws v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
 	golang.org/x/sys v0.0.0-20220622161953-175b2fd9d664 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
