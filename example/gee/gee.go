48c755bcd63a0deeecb9e2e7aa4784ecd2c4e01b
jojo-jie <18624275868@163.com>
2024-03-18 15:26:49 +0800

    gee context

--- example/gee/gee.go
+++ example/gee/gee.go
@@ -1,33 +1,25 @@
 package gee
 
 import (
-	"fmt"
 	"net/http"
 )
 
-type HandlerFunc func(http.ResponseWriter, *http.Request)
+type HandlerFunc func(*Context)
 
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router
 }
 
 func (engine *Engine) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	key := request.Method + "-" + request.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(writer, request)
-	} else {
-		writer.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(writer, "404 NOT FOUND: %s\n", request.URL)
-	}
+	engine.router.handle(newContext(writer, request))
 }
 
 func New() *Engine {
-	return &Engine{router: make(map[string]HandlerFunc)}
+	return &Engine{router: newRouter()}
 }
 
 func (engine *Engine) addRoute(method, pattern string, handler HandlerFunc) {
-	key := method + "-" + pattern
-	engine.router[key] = handler
+	engine.router.addRoute(method, pattern, handler)
 }
 
 func (engine *Engine) GET(pattern string, handler HandlerFunc) {
--- example/gee/gee_test.go
+++ example/gee/gee_test.go
@@ -1,20 +1,26 @@
 package gee
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 	"testing"
 )
 
 func TestGeeServer(t *testing.T) {
 	r := New()
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+
+	r.GET("/", func(c *Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
-	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
-		r, _ := json.Marshal(req.Header)
-		w.Write(r)
+	r.GET("/hello", func(c *Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.POST("/login", func(c *Context) {
+		c.JSON(http.StatusOK, H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
 	r.Run(":9000")
 }
