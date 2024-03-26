91189bf91e4332353ea7c86c40656d92df5fd718
jojo-jie <18624275868@163.com>
2024-03-26 16:37:20 +0800

    gee cache sync

---
+++ example/geeCache/geecache.go
@@ -0,0 +1,11 @@
+package geeCache
+
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+type GetterFunc func(key string) ([]byte, error)
+
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
