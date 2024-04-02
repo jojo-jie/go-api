36533b8d9e3838c5427605dd37dd6f5fbed2062c
jojo-jie <18624275868@163.com>
2024-04-02 15:20:24 +0800

    gee singleflight

--- example/geeCache/geecache.go
+++ example/geeCache/geecache.go
@@ -2,6 +2,7 @@ package geeCache
 
 import (
 	"fmt"
+	"go-api/example/geeCache/singleflight"
 	"log"
 	"sync"
 )
@@ -21,6 +22,8 @@ type Group struct {
 	name      string
 	getter    Getter
 	mainCache cache
+	peers     PeerPicker
+	loader    *singleflight.Group
 }
 
 var (
@@ -39,6 +42,7 @@ func NewGroup(name string, cacheBytes int, getter Getter) *Group {
 		name:      name,
 		getter:    getter,
 		mainCache: cache{cacheBytes: cacheBytes},
+		loader:    &singleflight.Group{},
 	}
 	groups[name] = g
 	return g
@@ -64,10 +68,6 @@ func (g *Group) Get(key string) (ByteView, error) {
 	return g.load(key)
 }
 
-func (g *Group) load(key string) (value ByteView, err error) {
-	return g.getLocally(key)
-}
-
 func (g *Group) getLocally(key string) (ByteView, error) {
 	bytes, err := g.getter.Get(key)
 	if err != nil {
@@ -81,4 +81,34 @@ func (g *Group) getLocally(key string) (ByteView, error) {
 
 func (g *Group) populateCache(key string, value ByteView) {
 	g.mainCache.add(key, value)
+}
+
+func (g *Group) RegisterPeers(peers PeerPicker) {
+	if g.peers != nil {
+		panic("RegisterPeerPicker called more than once")
+	}
+	g.peers = peers
+}
+
+func (g *Group) getFormPeer(peer PeerGetter, key string) (ByteView, error) {
+	bytes, err := peer.Get(g.name, key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	return ByteView{b: bytes}, nil
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	g.loader.Do(key, func() (interface{}, error) {
+		if g.peers != nil {
+			if peer, ok := g.peers.PickPeer(key); ok {
+				if value, err := g.getFormPeer(peer, key); err == nil {
+					return value, err
+				}
+				log.Println("[GeeCache] Failed to get from peer", err)
+			}
+		}
+		return g.getLocally(key)
+	})
+	return
 }
--- example/geeCache/peers.go
+++ example/geeCache/peers.go
@@ -1,7 +1,7 @@
 package geeCache
 
 type PeerPicker interface {
-	PickPeer(key string) (peer PeerPicker, ok bool)
+	PickPeer(key string) (peer PeerGetter, ok bool)
 }
 
 type PeerGetter interface {
