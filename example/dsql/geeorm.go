ee87dc682e3434b0ee525506ab73d63bb9daf974
jojo-jie <18624275868@163.com>
2024-04-04 10:34:05 +0800

    gee orm

---
+++ example/dsql/geeorm.go
@@ -0,0 +1,38 @@
+package dsql
+
+import (
+	"database/sql"
+	"go-api/example/dsql/log"
+	"go-api/example/dsql/session"
+)
+
+type Engine struct {
+	db *sql.DB
+}
+
+func NewEngine(driver, source string) (e *Engine, err error) {
+	db, err := sql.Open(driver, source)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	// Send a ping to make sure the database connection is alive.
+	if err = db.Ping(); err != nil {
+		log.Error(err)
+		return
+	}
+	e = &Engine{db: db}
+	log.Info("Connect database success")
+	return
+}
+
+func (engine *Engine) Close() {
+	if err := engine.db.Close(); err != nil {
+		log.Error("Failed to close database")
+	}
+	log.Info("Close database success")
+}
+
+func (engine *Engine) NewSession() *session.Session {
+	return session.New(engine.db)
+}
