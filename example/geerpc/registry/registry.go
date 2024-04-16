e6a8585a459be51cf0f919877e6be01c69a9e459
jojo-jie <18624275868@163.com>
2024-04-16 14:11:55 +0800

    gee rpc registry

---
+++ example/geerpc/registry/registry.go
@@ -0,0 +1,58 @@
+package registry
+
+import (
+	"sort"
+	"sync"
+	"time"
+)
+
+type GeeRegistry struct {
+	timeout time.Duration
+	mu      sync.Mutex // protect following
+	servers map[string]*ServerItem
+}
+
+type ServerItem struct {
+	Addr  string
+	start time.Time
+}
+
+const (
+	defaultPath    = "/_geerpc_/registry"
+	defaultTimeout = time.Minute * 5
+)
+
+func New(timeout time.Duration) *GeeRegistry {
+	return &GeeRegistry{
+		servers: make(map[string]*ServerItem),
+		timeout: timeout,
+	}
+}
+
+var DefaultGeeRegister = New(defaultTimeout)
+
+func (r *GeeRegistry) putServer(addr string) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	s := r.servers[addr]
+	if s == nil {
+		r.servers[addr] = &ServerItem{Addr: addr, start: time.Now()}
+	} else {
+		s.start = time.Now() // if exists, update start time to keep alive
+	}
+}
+
+func (r *GeeRegistry) aliveServers() []string {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	var alive []string
+	for addr, s := range r.servers {
+		if r.timeout == 0 || s.start.Add(r.timeout).After(time.Now()) {
+			alive = append(alive, addr)
+		} else {
+			delete(r.servers, addr)
+		}
+	}
+	sort.Strings(alive)
+	return alive
+}
