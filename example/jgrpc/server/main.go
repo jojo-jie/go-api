c4b189024dbd9f24f35bb0eb10b68b9fe827952b
jojo-jie <18624275868@163.com>
2024-05-25 11:30:56 +0800

    jgrpc url base64

--- example/jgrpc/server/main.go
+++ example/jgrpc/server/main.go
@@ -345,4 +345,28 @@ func atomicAs() {
 	fmt.Println(uEnc)
 	uDec, _ := base64.URLEncoding.DecodeString(uEnc)
 	fmt.Println(string(uDec))
+
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Println("sent job", j)
+	}
+	close(jobs)
+	fmt.Println("sent all jobs")
+	<-done
+	_, ok := <-jobs
+	fmt.Println("received more jobs:", ok)
 }
