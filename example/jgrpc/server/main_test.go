095bb878e25b37a0949380b799ccc7de969308e1
jojo-jie <18624275868@163.com>
2024-06-03 19:13:29 +0800

    jgrpc ato

--- example/jgrpc/server/main_test.go
+++ example/jgrpc/server/main_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
 	"reflect"
+	"sync/atomic"
 	"testing"
+	"time"
 )
 
 type MyType struct {
@@ -35,4 +39,73 @@ func TestComp(t *testing.T) {
 
 func compare(a, b any) bool {
 	return reflect.DeepEqual(a, b)
+}
+
+func TestAto(t *testing.T) {
+	var readOps uint64
+	var writeOps uint64
+
+	reads := make(chan readOp)
+	writes := make(chan writeOp)
+
+	go func() {
+		var state = make(map[int]int)
+		for {
+			select {
+			case read := <-reads:
+				read.resp <- state[read.key]
+			case write := <-writes:
+				state[write.key] = write.val
+				write.resp <- true
+			}
+		}
+	}()
+
+	for r := 0; r < 100; r++ {
+		go func() {
+			for {
+				read := readOp{
+					key:  rand.Intn(5),
+					resp: make(chan int)}
+				reads <- read
+				<-read.resp
+				atomic.AddUint64(&readOps, 1)
+				time.Sleep(time.Millisecond)
+			}
+		}()
+	}
+
+	// We start 10 writes as well, using a similar
+	// approach.
+	for w := 0; w < 10; w++ {
+		go func() {
+			for {
+				write := writeOp{
+					key:  rand.Intn(5),
+					val:  rand.Intn(100),
+					resp: make(chan bool)}
+				writes <- write
+				<-write.resp
+				atomic.AddUint64(&writeOps, 1)
+				time.Sleep(time.Millisecond)
+			}
+		}()
+	}
+
+	time.Sleep(time.Second)
+
+	readOpsFinal := atomic.LoadUint64(&readOps)
+	fmt.Println("readOps:", readOpsFinal)
+	writeOpsFinal := atomic.LoadUint64(&writeOps)
+	fmt.Println("writeOps:", writeOpsFinal)
+}
+
+type readOp struct {
+	key  int
+	resp chan int
+}
+type writeOp struct {
+	key  int
+	val  int
+	resp chan bool
 }
