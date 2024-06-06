a86367c2c4bae062f3a5cf91e765bae9ca639b6d
jojo-jie <18624275868@163.com>
2024-06-06 15:48:49 +0800

    jgrpc timeout ctx

--- example/jgrpc/server/main_test.go
+++ example/jgrpc/server/main_test.go
@@ -126,6 +126,29 @@ func TestCtx(t *testing.T) {
 	cancel()
 	g.Wait()
 	t.Logf("doSomething: finished\n")
+
+	c1 := make(chan string, 1)
+	go func() {
+		time.Sleep(2 * time.Second)
+		c1 <- "result 1"
+	}()
+	select {
+	case res := <-c1:
+		fmt.Println(res)
+	case <-time.After(1 * time.Second):
+		fmt.Println("timeout 1")
+	}
+	c2 := make(chan string, 1)
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "result 2"
+	}()
+	select {
+	case res := <-c2:
+		fmt.Println(res)
+	case <-time.After(3 * time.Second):
+		fmt.Println("timeout 2")
+	}
 }
 
 func doAnother(ctx context.Context, printCh <-chan int, t *testing.T) {
