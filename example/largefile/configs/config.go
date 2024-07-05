7f05728e8a663d66df53b4e8cfa4d0fab7017149
jojo-jie <18624275868@163.com>
2024-07-05 17:37:25 +0800

    largefile configs init

---
+++ example/largefile/configs/config.go
@@ -0,0 +1,41 @@
+package configs
+
+import (
+	"embed"
+	"gopkg.in/yaml.v3"
+	"os"
+	"strings"
+)
+
+//go:embed config.yaml
+var configs embed.FS
+
+type Config struct {
+	Minio MinioConfig `yaml:"minio"`
+}
+
+type MinioConfig struct {
+	Url        string `yaml:"url"`
+	AccessKey  string `yaml:"access_key"`
+	SecretKey  string `yaml:"secret_key"`
+	Api        string `yaml:"api"`
+	Path       string `yaml:"path"`
+	BucketName string `yaml:"bucket_name"`
+}
+
+func New() *Config {
+	return &Config{}
+}
+
+func (c *Config) Init() error {
+	configStr, err := configs.ReadFile("config.yaml")
+	if os.IsNotExist(err) {
+		return err
+	}
+	decoder := yaml.NewDecoder(strings.NewReader(string(configStr)))
+	err = decoder.Decode(c)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- example/largefile/main.go
+++ example/largefile/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"largefile/configs"
+)
 
+func main() {
+	c := configs.New()
+	err := c.Init()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(c.Minio.BucketName)
 }
