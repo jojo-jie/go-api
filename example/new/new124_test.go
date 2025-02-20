1a7d9829c8a66340517796baf5a5d1b11351bcc9
jojo-jie <2725445401@qq.com>
2025-02-20 17:21:34 +0800

    time after

---
+++ example/new/new124_test.go
@@ -0,0 +1,36 @@
+package new
+
+import (
+	"testing"
+	"time"
+)
+
+func TestNew124(tt *testing.T) {
+	timeout := 50 * time.Millisecond
+	t := time.NewTimer(timeout)
+	time.Sleep(100 * time.Millisecond)
+	start := time.Now()
+	t.Reset(timeout)
+	<-t.C
+	tt.Logf("range：%dms\n", time.Since(start).Milliseconds())
+}
+
+func TestTimeA(tt *testing.T) {
+	ch := make(chan int, 10)
+	go func() {
+		i := 1
+		for {
+			i++
+			ch <- i
+		}
+	}()
+
+	for {
+		select {
+		case i := <-ch:
+			tt.Logf("done:%d", i)
+		case <-time.After(3 * time.Minute):
+			tt.Logf("现在是：%d，我脑子进煎鱼了！", time.Now().Unix())
+		}
+	}
+}
