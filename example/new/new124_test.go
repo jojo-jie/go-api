16c9f20727a25501a41800f41d85ee6f0e1bc16d
jojo-jie <2725445401@qq.com>
2025-03-28 10:50:01 +0800

    worker

--- example/new/new124_test.go
+++ example/new/new124_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"sync"
 	"testing"
 	"time"
 )
@@ -132,5 +133,42 @@ func New() Service {
 		next: validator{
 			next: service{},
 		},
+	}
+}
+
+func TestWorkerPool(t *testing.T) {
+	tasks := make(chan int, 10)
+	results := make(chan int, 10)
+	var wg sync.WaitGroup
+
+	// Create 3 workers
+	for i := 1; i <= 3; i++ {
+		wg.Add(1)
+		go worker(i, tasks, results, &wg)
+	}
+
+	// Send tasks
+	for i := 1; i <= 5; i++ {
+		tasks <- i
+	}
+	close(tasks)
+
+	// Wait for all workers to finish
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	// Collect results
+	for r := range results {
+		t.Log("Result:", r)
+	}
+}
+
+func worker(id int, tasks <-chan int, results chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for t := range tasks {
+		// Do some work, e.g., multiply by 2
+		results <- t * 2
 	}
 }
