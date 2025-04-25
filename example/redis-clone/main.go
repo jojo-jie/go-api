508183917c4a99eb1c968044d02b3f4f5da92455
jojo-jie <2725445401@qq.com>
2025-04-25 13:59:20 +0800

    replayAOF server

--- example/redis-clone/main.go
+++ example/redis-clone/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
+	"net"
 	"os"
 	"redis-clone/store"
 	"strings"
@@ -10,7 +12,7 @@ import (
 )
 
 type Server struct {
-	cache *store.Cache // Cache used to store key-value pairs
+	cache *store.Cache
 }
 
 /*
@@ -19,25 +21,21 @@ It creates a Cache instance with AOF persistence enabled, reading from the speci
 It also replays the AOF file to restore the cache state and starts a background cleaning process for the cache.
 */
 func NewServer(aofFilename string) (*Server, error) {
-	// Initialize AOF persistence
 	persist, err := store.NewAOF(aofFilename)
 	if err != nil {
 		return nil, err
 	}
 
-	// Create the cache with a maximum size of 5 items and enable AOF persistence
 	cache := store.NewCache(5, persist)
 
-	// Replay the AOF file to restore the cache state
 	if err := replayAOF(aofFilename, cache); err != nil {
 		return nil, err
 	}
 
-	// Start the cache cleaning server in the background
 	go cache.StartCleaningServer()
 
 	return &Server{
-		cache: cache, // Initialize the server with the cache
+		cache: cache,
 	}, nil
 }
 
@@ -64,4 +62,84 @@ func replayAOF(aofFilename string, cache *store.Cache) error {
 	}
 
 	return nil
+}
+
+func (s *Server) HandleConnection(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+
+	for {
+		// Read the incoming message from the client
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		message = strings.TrimSpace(message)
+
+		tokens := strings.Split(message, " ")
+
+		if len(tokens) < 2 {
+			conn.Write([]byte("Invalid command\n"))
+			continue
+		}
+
+		cmd := strings.ToUpper(tokens[0])
+
+		switch cmd {
+		case "SET":
+
+			if len(tokens) < 3 {
+				conn.Write([]byte("Usage: SET key value\n"))
+				continue
+			}
+
+			s.cache.Set(tokens[1], tokens[2], time.Hour, false)
+			conn.Write([]byte("OK\n"))
+
+		case "GET":
+			key := tokens[1]
+			if item, exists := s.cache.Get(key); !exists {
+				conn.Write([]byte("nil\n"))
+			} else {
+				conn.Write([]byte(item + "\n"))
+			}
+
+		default:
+			conn.Write([]byte("Unknown Command\n"))
+		}
+	}
+}
+
+func (s *Server) Start(address string) {
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		panic(err)
+	}
+
+	defer listener.Close()
+
+	fmt.Println("Cache server running on port", address)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		go s.HandleConnection(conn)
+	}
+}
+
+func main() {
+	aofFilename := "cache.aof"
+
+	server, err := NewServer(aofFilename)
+	if err != nil {
+		log.Fatalf("Error initializing server: %v", err)
+	}
+
+	server.Start(":6379")
 }
