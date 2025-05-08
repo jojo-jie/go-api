004e339afcb0204e230a77c7211c6ee543bee618
jojo-jie <2725445401@qq.com>
2025-05-08 17:22:48 +0800

    GracefulShutdown

--- example/new/new124_test.go
+++ example/new/new124_test.go
@@ -3,14 +3,21 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
+	"log"
+	"net"
+	"net/http"
+	"os/signal"
 	"reflect"
 	"runtime"
 	"strings"
 	"sync"
+	"sync/atomic"
+	"syscall"
 	"testing"
 	"time"
 )
@@ -431,4 +438,75 @@ func TestIterator(t *testing.T) {
 		user := iterator.getNext()
 		t.Logf("User: %v\n", user)
 	}
+}
+
+const (
+	_shutdownPeriod      = 15 * time.Second
+	_shutdownHardPeriod  = 3 * time.Second
+	_readinessDrainDelay = 5 * time.Second
+)
+
+var isShuttingDown atomic.Boo
+var isShuttingDown atomic.Bool
+
+func TestGracefulShutdown(t *testing.T) {
+	// Setup signal context
+	rootCtx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	// Readiness endpoint
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		if isShuttingDown.Load() {
+			http.Error(w, "Shutting down", http.StatusServiceUnavailable)
+			return
+		}
+		fmt.Fprintln(w, "OK")
+	})
+
+	// Sample business logic
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		select {
+		case <-time.After(2 * time.Second):
+			fmt.Fprintln(w, "Hello, world!")
+		case <-r.Context().Done():
+			http.Error(w, "Request cancelled.", http.StatusRequestTimeout)
+		}
+	})
+
+	// Ensure in-flight requests aren't cancelled immediately on SIGTERM
+	ongoingCtx, stopOngoingGracefully := context.WithCancel(context.Background())
+	server := &http.Server{
+		Addr: ":8080",
+		BaseContext: func(_ net.Listener) context.Context {
+			return ongoingCtx
+		},
+	}
+
+	go func() {
+		log.Println("Server starting on :8080.")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			panic(err)
+		}
+	}()
+
+	// Wait for signal
+	<-rootCtx.Done()
+	stop()
+	isShuttingDown.Store(true)
+	log.Println("Received shutdown signal, shutting down.")
+
+	// Give time for readiness check to propagate
+	time.Sleep(_readinessDrainDelay)
+	log.Println("Readiness check propagated, now waiting for ongoing requests to finish.")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), _shutdownPeriod)
+	defer cancel()
+	err := server.Shutdown(shutdownCtx)
+	stopOngoingGracefully()
+	if err != nil {
+		log.Println("Failed to wait for ongoing requests to finish, waiting for forced cancellation.")
+		time.Sleep(_shutdownHardPeriod)
+	}
+
+	log.Println("Server shut down gracefully.")
 }
