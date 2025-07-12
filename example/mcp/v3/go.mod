01c83ff1daff986c2fa627ad7003bc9be6f8e40c
jojo-jie <2725445401@qq.com>
2025-07-12 22:46:21 +0800

    modelcontextprotocol

--- example/mcp/v3/go.mod
+++ example/mcp/v3/go.mod
@@ -2,4 +2,6 @@ module mcp.v3
 
 go 1.24.1
 
-require github.com/modelcontextprotocol/go-sdk v0.1.0 // indirect
+require github.com/modelcontextprotocol/go-sdk v0.2.0
+
+require github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
--- example/mcp/v3/main.go
+++ example/mcp/v3/main.go
@@ -1,4 +1,4 @@
-package v3
+package main
 
 import (
 	"context"
@@ -7,11 +7,17 @@ import (
 	"log"
 )
 
+/*
+printf '%s\n' \
+'{"jsonrpc":"2.0","id":1,"method":"initialize","params":{"clientInfo":{"name":"test-cli","version":"0.1"},"protocolVersion":"2025-03-26"}}' \
+'{"jsonrpc":"2.0","method":"notifications/initialized","params":{}}' \
+'{"jsonrpc":"2.0","id":2,"method":"tools/call","params":{"name":"greet","arguments":{"name":"Go MCP Enthusiast"}}}' \
+| go run main.go
+*/
 func main() {
-	server := mcp.NewServer("greeter-server", "1.0.0", nil)
-	server.AddTools(
-		mcp.NewServerTool("greet", "Say hi to someone", SayHi),
-	)
+	server := mcp.NewServer(&mcp.Implementation{Name: "greeter", Version: "v1.0.0"}, nil)
+	mcp.AddTool(server, &mcp.Tool{Name: "greet", Description: "say hi"}, SayHi)
+
 	log.Println("Greeter server running over stdio...")
 	if err := server.Run(context.Background(), mcp.NewStdioTransport()); err != nil {
 		log.Fatalf("Server run failed: %+v", err)
@@ -23,7 +29,7 @@ type HiParams struct {
 }
 
 func SayHi(ctx context.Context, session *mcp.ServerSession, c *mcp.CallToolParamsFor[HiParams]) (*mcp.CallToolResultFor[any], error) {
-	resultText := fmt.Sprintf("Hi %s", c.Arguments.Name)
+	resultText := fmt.Sprintf("Hi %s Name==%s", c.Arguments.Name, c.Name)
 	return &mcp.CallToolResultFor[any]{
 		Content: []mcp.Content{
 			&mcp.TextContent{Text: resultText},
